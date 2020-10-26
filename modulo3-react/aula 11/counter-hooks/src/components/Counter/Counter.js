f7d5061b7aa50787e7721192c28fc368e5bec01e
Estrela <estrela.steinkirch@gmail.com>
2020-10-26 18:13:14 +0100

    add hooks on Counter and App.js

--- modulo3-react/aula 11/counter-hooks/src/components/Counter/Counter.js
+++ modulo3-react/aula 11/counter-hooks/src/components/Counter/Counter.js
@@ -1,45 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import css from "./counter.module.css";
 import IncrementButton from "./IncrementButton";
 import DecrementButton from "./DecrementButton";
 import Value from "./Value";
 import Steps from "./Steps";
 
-export default class Counter extends Component {
-  constructor() {
-    super();
-    //deve ter this antes dos atributos
-    this.state = {
-      currentCounter: 2,
-      steps: 0,
-    };
-  }
+export default function Counter() {
+  const [currentCounter, setCurrentCounter] = useState(2);
+  const [steps, setSteps] = useState(0);
 
-  handleButtonClick = (clickType) => {
-    //tendo acesso a eles antes do setState, nao pode modificar ele diretamente
-    const { currentCounter, steps } = this.state;
-    this.setState({
-      currentCounter:
-        clickType === "+" ? currentCounter + 1 : currentCounter - 1,
-      steps: steps + 1,
-    });
+  const handleButtonClick = (clickType) => {
+    const counter = clickType === "+" ? currentCounter + 1 : currentCounter - 1;
+    setCurrentCounter(counter);
+    setSteps(steps + 1);
   };
 
-  render() {
-    const { currentCounter, steps } = this.state;
-    return (
-      <div className={css.counterContainer}>
-     
-        <DecrementButton onDecrement={this.handleButtonClick} />
+  return (
+    <div className={css.counterContainer}>
+      <DecrementButton onDecrement={handleButtonClick} />
 
-        <Value value={currentCounter}/>
+      <Value value={currentCounter} />
 
-        <IncrementButton onIncrement={this.handleButtonClick} />
+      <IncrementButton onIncrement={handleButtonClick} />
 
-       <Steps currentStep={steps}/>
-      </div>
-    );
-  }
+      <Steps currentStep={steps} />
+    </div>
+  );
 }
 
 //criar classe com rcc tab
